9dd9d2dc7ff0e13fa986cbc548d3d73b4dca13a6
牛飞 <niufei@staff.sina.com.cn>
2018-04-07 22:40:07 +0800

    提交评论

---
+++ app/src/main/java/com/jung/finance/bean/CommentCreateModel.java
@@ -0,0 +1,18 @@
+package com.jung.finance.bean;
+
+
+/***
+ *
+ * @Copyright 2018
+ *
+ * @TODO
+ *
+ * @author niufei
+ *
+ *
+ * @date 2018/4/7. 下午10:25
+ *
+ *
+ */
+public class CommentCreateModel {
+}
